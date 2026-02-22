94f1f4b52124014767117dd67c102202a1c7de93
khengleng <khenglengt@gmail.com>
2026-02-22 08:36:36 +0700

    Enable gunicorn logging

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -10,3 +10,6 @@ graceful_timeout = int(os.getenv("GUNICORN_GRACEFUL_TIMEOUT", "30"))
 keepalive = int(os.getenv("GUNICORN_KEEPALIVE", "5"))
 max_requests = int(os.getenv("GUNICORN_MAX_REQUESTS", "5000"))
 max_requests_jitter = int(os.getenv("GUNICORN_MAX_REQUESTS_JITTER", "500"))
+accesslog = "-"
+errorlog = "-"
+loglevel = "info"
